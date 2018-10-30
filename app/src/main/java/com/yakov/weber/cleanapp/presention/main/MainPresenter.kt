0bca13c603cbe5d0e86e1d64886880780a3bf66a
yakov <cen7ur1on007@gmail.com>
2018-10-30 23:31:18 +0200

    add main fragment and bottom menu

---
+++ app/src/main/java/com/yakov/weber/cleanapp/presention/main/MainPresenter.kt
@@ -0,0 +1,13 @@
+package com.yakov.weber.cleanapp.presention.main
+
+import com.arellomobile.mvp.InjectViewState
+import com.yakov.weber.cleanapp.presention.global.BasePresenter
+import javax.inject.Inject
+
+/**
+ * Created on 30.10.18
+ * @author YWeber
+ * project CleanApp */
+@InjectViewState
+class MainPresenter @Inject constructor() : BasePresenter<MainView>() {
+}
---
+++ app/src/main/java/com/yakov/weber/cleanapp/presention/main/MainView.kt
@@ -0,0 +1,12 @@
+package com.yakov.weber.cleanapp.presention.main
+
+import com.arellomobile.mvp.MvpView
+
+/**
+ * Created on 30.10.18
+ * @author YWeber
+ * project CleanApp */
+
+interface MainView : MvpView {
+
+}
